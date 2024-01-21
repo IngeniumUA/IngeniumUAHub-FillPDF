73aa3041319af7bd1052276e59e0b678e0547f07
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2024-01-21 22:43:48 +0100

    Updated documentation of attachments dictionary even further adding params

--- Mailing/Mailing.py
+++ Mailing/Mailing.py
@@ -17,6 +17,11 @@ class AttachmentsDictionary(TypedDict):
     """
     A dictionary that stores all the attachments, also used for type hinting.
     MIME types: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+
+    :param attachment: The attachment data as bytes or the path as a string
+    :param filename: The desired name of the attachment file
+    :param mime_maintype: The MIME main type
+    :param mime_subtype: The MIME sub type
     """
     attachment: str | bytes
     filename: str
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Mailing",
-    version="21.0",
+    version="22.0",
     description="Implements the Gmail API to send mails",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
