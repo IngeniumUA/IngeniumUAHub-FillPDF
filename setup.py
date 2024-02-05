4af124e9b98fd50664befcae47000e26db1d0522
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2024-02-05 18:04:11 +0100

    Fixed a mathematical expression

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FillPDF",
-    version="24.0",
+    version="25.0",
     description="Used to automatically fill PDF's",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
