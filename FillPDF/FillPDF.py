74b58ac0e132a7165f5d1746bbc41c21785e0f9f
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2024-02-05 15:40:30 +0100

    Add FillPDF class otherwise functions could not be called

--- FillPDF/FillPDF.py
+++ FillPDF/FillPDF.py
@@ -17,72 +17,73 @@ class OnkostennotaGegevensDictionary(TypedDict):
     datum: str
 
 
-def fillOnkostennota(volgnummer: str | None, gegevens: OnkostennotaGegevensDictionary | None,
-                     onkosten: list[OnkostennotaOnkostenDictionary] | None, betaaldatum: str | None,
-                     savepath: str) -> None:
-    # Standaard variabelen, worden uit PDF gehaald
-    vervoersonkosten_vergoeding = 0.42
-    boekhoudpost_vervoer = 615000
-    max_onkosten = 16
+class FillPDF:
+    def fillOnkostennota(volgnummer: str | None, gegevens: OnkostennotaGegevensDictionary | None,
+                         onkosten: list[OnkostennotaOnkostenDictionary] | None, betaaldatum: str | None,
+                         savepath: str) -> None:
+        # Standaard variabelen, worden uit PDF gehaald
+        vervoersonkosten_vergoeding = 0.42
+        boekhoudpost_vervoer = 615000
+        max_onkosten = 16
 
-    reader = PdfReader("Templates/Onkostennota.pdf")
-    writer = PdfWriter()
-    writer.append(reader)
+        reader = PdfReader("Templates/Onkostennota.pdf")
+        writer = PdfWriter()
+        writer.append(reader)
 
-    # Volgnummer invullen
-    if volgnummer is not None:
-        writer.update_page_form_field_values(
-            writer.pages[0],
-            {"Volgnummer": volgnummer},
-            auto_regenerate=False,
-        )
-    # Betaaldatum invullen
-    if betaaldatum is not None:
-        writer.update_page_form_field_values(
-            writer.pages[0],
-            {"DatumVoldaan": volgnummer},
-            auto_regenerate=False,
-        )
-    # Gegevens invullen
-    if gegevens is not None:
-        writer.update_page_form_field_values(
-            writer.pages[0],
-            {"Voornaam": gegevens["voornaam"], "Naam": gegevens["naam"], "Email": gegevens["email"],
-             "Rekeningnummer": gegevens["rekeningnummer"], "DatumGemaakt": gegevens["datum"]},
-            auto_regenerate=False,
-        )
+        # Volgnummer invullen
+        if volgnummer is not None:
+            writer.update_page_form_field_values(
+                writer.pages[0],
+                {"Volgnummer": volgnummer},
+                auto_regenerate=False,
+            )
+        # Betaaldatum invullen
+        if betaaldatum is not None:
+            writer.update_page_form_field_values(
+                writer.pages[0],
+                {"DatumVoldaan": volgnummer},
+                auto_regenerate=False,
+            )
+        # Gegevens invullen
+        if gegevens is not None:
+            writer.update_page_form_field_values(
+                writer.pages[0],
+                {"Voornaam": gegevens["voornaam"], "Naam": gegevens["naam"], "Email": gegevens["email"],
+                 "Rekeningnummer": gegevens["rekeningnummer"], "DatumGemaakt": gegevens["datum"]},
+                auto_regenerate=False,
+            )
 
-    # Onkosten invullen
-    i = 1
-    totaal = 0
-    if onkosten is not None:
-        if len(onkosten) > max_onkosten:
-            raise Exception("Er zijn meer onkosten dan in de nota passen.")
-        for onkost in onkosten:
-            # Field names
-            boekhoudpost_field = "Boekhoudpost" + str(i)
-            beschrijving_field = "Beschrijving" + str(i)
-            prijs_field = "Prijs" + str(i)
+        # Onkosten invullen
+        i = 1
+        totaal = 0
+        if onkosten is not None:
+            if len(onkosten) > max_onkosten:
+                raise Exception("Er zijn meer onkosten dan in de nota passen.")
+            for onkost in onkosten:
+                # Field names
+                boekhoudpost_field = "Boekhoudpost" + str(i)
+                beschrijving_field = "Beschrijving" + str(i)
+                prijs_field = "Prijs" + str(i)
 
-            if onkost["boekhoudpost"] == boekhoudpost_vervoer:
-                eindprijs = onkost["prijs_of_km"] * vervoersonkosten_vergoeding
-            else:
-                eindprijs = onkost["prijs_of_km"]
-            totaal += eindprijs
+                if onkost["boekhoudpost"] == boekhoudpost_vervoer:
+                    eindprijs = onkost["prijs_of_km"] * vervoersonkosten_vergoeding
+                else:
+                    eindprijs = onkost["prijs_of_km"]
+                totaal += eindprijs
+
+                writer.update_page_form_field_values(
+                    writer.pages[0],
+                    {boekhoudpost_field: onkost["boekhoudpost"], beschrijving_field: onkost["beschrijving"],
+                     prijs_field: str(round(eindprijs, 2))},
+                    auto_regenerate=False,
+                )
+                i += 1
 
             writer.update_page_form_field_values(
                 writer.pages[0],
-                {boekhoudpost_field: onkost["boekhoudpost"], beschrijving_field: onkost["beschrijving"],
-                 prijs_field: str(round(eindprijs, 2))},
+                {"Totaal": totaal},
                 auto_regenerate=False,
             )
-            i += 1
 
-        writer.update_page_form_field_values(
-            writer.pages[0],
-            {"Totaal": totaal},
-            auto_regenerate=False,
-        )
-
-    with open(savepath, "wb") as output_stream:
-        writer.write(output_stream)
+        with open(savepath, "wb") as output_stream:
+            writer.write(output_stream)
