7f52cc08b23f94115b3b74a698285d5853ec8f23
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2024-02-05 17:04:44 +0100

    Vervoervergoeding is nu een variabele die moet meegegeven worden en documentatie is toegevoegd en template is geupdate

--- FillPDF/FillPDF.py
+++ FillPDF/FillPDF.py
@@ -6,12 +6,28 @@ from pypdf import PdfReader, PdfWriter
 
 
 class OnkostennotaOnkostenDictionary(TypedDict):
+    """
+    Dictionary die alle gegevens van de onkost opslaat, wordt ook gebruikt voor type hinting.
+
+    :param boekhoudpost: De boekhoudpost waar de kost bij hoort.
+    :param beschrijving: Beschrijving van de onkost.
+    :param prijs_of_km: Aantal gereden kilometers in geval van transport, anders de prijs van de onkost.
+    """
     boekhoudpost: str
     beschrijving: str
     prijs_of_km: Decimal
 
 
 class OnkostennotaGegevensDictionary(TypedDict):
+    """
+    Dictionary die alle gegevens van de persoon die de onkosten gemaakt heeft opslaat, wordt ook gebruikt voor type hinting.
+
+    :param voornaam: Voornaam van de persoon.
+    :param naam: Achternaam van de persoon.
+    :param email: Email van de persoon.
+    :param rekeningnummer: Rekeningnummer van de persoon.
+    :param datum: Datum dat de persoon de kosten heeft gemaakt.
+    """
     voornaam: str
     naam: str
     email: str
@@ -21,12 +37,22 @@ class OnkostennotaGegevensDictionary(TypedDict):
 
 class FillPDF:
     def __init__(self) -> None:
-        self.vervoersonkosten_vergoeding = Decimal(0.42)
         self.boekhoudpost_vervoer = "615000"
         self.max_onkosten = 16
 
     def fillOnkostennota(self, savepath: str, volgnummer: str = None, gegevens: OnkostennotaGegevensDictionary = None,
-                         onkosten: list[OnkostennotaOnkostenDictionary] = None, betaaldatum: str = None, ) -> None:
+                         onkosten: list[OnkostennotaOnkostenDictionary] = None, betaaldatum: str = None,
+                         vervoersonkosten_vergoeding: Decimal = None) -> None:
+        """
+        Functie die automatisch de onkostennota invult.
+
+        :param savepath: Waar de onkostennota op te slaan.
+        :param volgnummer: Volgnummer van de onkostennota.
+        :param gegevens: Gegevens van de persoon die de onkosten gedaan heeft.
+        :param onkosten: Lijst met de onkosten als dictionary. Maximaal 16.
+        :param betaaldatum: Wanneer onkostennota effectief betaald wordt.
+        :param vervoersonkosten_vergoeding: Hoeveel er per km vergoed wordt.
+        """
         # Standaard variabelen, worden uit PDF gehaald
         template_dir = os.path.join(os.path.dirname(__file__), "templates")
         reader = PdfReader(os.path.join(template_dir, "Onkostennota.pdf"))
@@ -36,7 +62,7 @@ class FillPDF:
         # Vervoersvergoeding invullen
         writer.update_page_form_field_values(
             writer.pages[0],
-            {"Vervoersonkosten": str(round(self.vervoersonkosten_vergoeding, 2))},
+            {"Vervoersonkosten": str(round(vervoersonkosten_vergoeding, 4))},
             auto_regenerate=False,
         )
 
@@ -76,7 +102,9 @@ class FillPDF:
                 prijs_field = "Prijs" + str(i)
 
                 if onkost["boekhoudpost"] == self.boekhoudpost_vervoer:
-                    eindprijs = onkost["prijs_of_km"] * self.vervoersonkosten_vergoeding
+                    if vervoersonkosten_vergoeding is None:
+                        raise Exception("Vervoersonkosten vergoeding heeft geen waarde gekregen.")
+                    eindprijs = onkost["prijs_of_km"] * vervoersonkosten_vergoeding
                 else:
                     eindprijs = onkost["prijs_of_km"]
                 writer.update_page_form_field_values(
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FillPDF",
-    version="18.0",
+    version="19.0",
     description="Used to automatically fill PDF's",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
