2e9d431b1289b2b17aa458a4e24909fe3fef0262
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2024-02-05 16:25:16 +0100

    Trying something

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="FillPDF",
-    version="10.0",
+    version="11.0",
     description="Used to automatically fill PDF's",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
     packages=find_packages(),  # same as name
+    include_package_data=True,
     package_data={'FillPDF': ["Templates/*"]},
     install_requires=[
         "pypdf",
