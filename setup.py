77459cfe3fc27208e7938b926376b4cf8403e625
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2024-02-05 15:38:41 +0100

    Fixed an issue in setup.py where requirements where not typed as a string

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-import pypdf
 from setuptools import setup
 
 setup(
     name="FillPDF",
-    version="1.0",
+    version="2.0",
     description="Used to automatically fill PDF's",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
     packages=["FillPDF"],  # same as name
     install_requires=[
-        pypdf
+        "pypdf",
     ],  # external packages as dependencies
 )
