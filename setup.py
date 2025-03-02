d8e185dd32a6cb4a5651249ece2c6915ee1bb00b
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2025-03-02 23:14:26 +0100

    Testing linting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fillpdf",
-    version="73.0",
+    version="74.0",
     description="Used to automatically fill PDF's",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
